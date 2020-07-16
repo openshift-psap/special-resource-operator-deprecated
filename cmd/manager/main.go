6ab4827f2ab207a1fd05dc2e4c75cade31ab5499
Zvonko Kosic <zkosic@redhat.com>
2020-07-16 15:47:54 -0300

    Making gofmt happy

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -117,12 +117,12 @@ func main() {
 		log.Error(err, "")
 		os.Exit(1)
 	}
-	
+
 	if err := specialresource.AddConfiglient(cfg); err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
-	
+
 	// Setup all Controllers
 	if err := controller.AddToManager(mgr); err != nil {
 		log.Error(err, "")
