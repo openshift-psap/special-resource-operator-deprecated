a1498af04a431a81645e21e78222a225a854a2d1
Zvonko Kaiser <zkaiser@redhat.com>
2020-10-26 13:59:28 +0000

    Updated zapr.go see notes.txt

--- vendor/github.com/go-logr/zapr/zapr.go
+++ vendor/github.com/go-logr/zapr/zapr.go
@@ -49,6 +49,20 @@ type noopInfoLogger struct{}
 func (l *noopInfoLogger) Enabled() bool                   { return false }
 func (l *noopInfoLogger) Info(_ string, _ ...interface{}) {}
 
+func (l *noopInfoLogger) Error(err error, msg string, keysAndValues ...interface{}) {}
+func (l *noopInfoLogger) V(level int) logr.Logger                                   { return &noopInfoLogger{} }
+func (l *noopInfoLogger) WithValues(keysAndValues ...interface{}) logr.Logger {
+	return &noopInfoLogger{}
+}
+func (l *noopInfoLogger) WithName(name string) logr.Logger { return &noopInfoLogger{} }
+
+func (l *infoLogger) Error(err error, msg string, keysAndValues ...interface{}) {}
+func (l *infoLogger) V(level int) logr.Logger                                   { return &infoLogger{} }
+func (l *infoLogger) WithValues(keysAndValues ...interface{}) logr.Logger {
+	return &infoLogger{}
+}
+func (l *infoLogger) WithName(name string) logr.Logger { return &infoLogger{} }
+
 var disabledInfoLogger = &noopInfoLogger{}
 
 // NB: right now, we always use the equivalent of sugared logging.
